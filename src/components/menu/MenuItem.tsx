6b08f6fbbc4840a6d7e284a9718b258344b0aea2
majdbassoumi <mbbassoumi@gmail.com>
2019-12-23 22:18:28 +0200

    fix in menu item

--- src/components/menu/MenuItem.tsx
+++ src/components/menu/MenuItem.tsx
@@ -6,19 +6,17 @@ import {Link} from 'react-router-dom';
 const MenuItem = ({link, icon, text, closeMenu}: MenuItemProps) => {
     return (
         <div className="pb-3">
-            <div
-                className="p-2 rounded cursor-pointer hover:bg-indigo-400  text-gray-400 hover:text-white font-bold text-xl tracking-wide items-center"
-                onClick={closeMenu}
+            <Link to={link}
+                  className="flex p-2 rounded cursor-pointer hover:bg-indigo-400  text-gray-400 hover:text-white font-bold text-xl tracking-wide items-center"
+                  onClick={closeMenu}
             >
-                <Link to={link} className="flex" >
-                    <div className="pr-2">
-                        <FontAwesomeIcon icon={icon} size="lg"/>
-                    </div>
-                    <div className="">
-                        {text}
-                    </div>
-                </Link>
-            </div>
+                <div className="pr-2">
+                    <FontAwesomeIcon icon={icon} size="lg"/>
+                </div>
+                <div className="">
+                    {text}
+                </div>
+            </Link>
         </div>
     );
 };
