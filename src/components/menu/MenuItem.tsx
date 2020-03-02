9c289d5d94bf724aa42f35fea7bfd974b0aefb17
majdbassoumi <mbbassoumi@gmail.com>
2020-03-02 22:32:34 +0200

    enhance menu ui

--- src/components/menu/MenuItem.tsx
+++ src/components/menu/MenuItem.tsx
@@ -2,16 +2,26 @@ import React from 'react';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {MenuItemProps} from './types';
 import {Link} from 'react-router-dom';
+import classNames from 'classnames';
+
 
 const MenuItem = ({link, icon, text, closeMenu}: MenuItemProps) => {
+
+    const linkStyles = classNames(
+        'flex p-2  rounded-bl rounded-tl rounded-r-full cursor-pointer hover:bg-indigo-400 text-gray-400 hover:text-white  tracking-wide items-center outline-none focus:shadow-outline ',
+        {
+            'bg-indigo-400' : (window.location.pathname === link),
+        }
+    );
+
     return (
-        <div className="pb-3">
+        <div className="pb-1">
             <Link to={link}
-                  className="flex p-2 rounded cursor-pointer hover:bg-indigo-400  text-gray-400 hover:text-white font-bold text-xl tracking-wide items-center"
+                  className={linkStyles}
                   onClick={closeMenu}
             >
-                <div className="pr-2">
-                    <FontAwesomeIcon icon={icon} size="lg"/>
+                <div className="w-12 ml-2 flex">
+                    <FontAwesomeIcon icon={icon} size="sm" fixedWidth={true}/>
                 </div>
                 <div className="">
                     {text}
